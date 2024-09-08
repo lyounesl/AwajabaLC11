01707cae1e1122a006a7a2162a04ddc45f257ad4
younes <younes.miftah.sio@gmail.com>
2024-09-08 21:28:27 +0200

    C3

--- Repas.java
+++ Repas.java
@@ -5,4 +5,9 @@ public class Repas {
     private int numero;
     private LocalDate date;
 
+    public Repas(int numero, LocalDate date) {
+        this.numero = numero;
+        this.date = date;
+    }
+    
 }
